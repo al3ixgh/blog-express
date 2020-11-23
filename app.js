125f6a06c566f4f4ed7827752117e5f0d0fb49ce
aleix <al3ixgh@gmail.com>
2020-11-23 12:58:18 +0100

    primer commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const Blog = require('./models/blog');
+
+require('./db/mongoose')
+const blogRouter = require('./routers/test')
+
+const port = process.env.PORT
+
+// express app
+const app = express();
+
+// listen for requests
+app.listen(port, () => {
+  console.log(`Server listening to port ${port}`)
+});
+
+// register view engine
+app.set('view engine', 'ejs');
+
+// middleware & static files
+app.use(express.static('public'));
+
+app.get('/', async (req, res) => {
+  try{
+    const blogs =await Blog.find({})
+    res.render('index', { blogs: blogs, titulo: 'Home' });
+  }catch (e){
+
+  res.render('index', { blogs: [], titulo: 'Índice' });
+  }
+});
+
+app.get('/form', (req, res)=>{
+  res.render('formulario', {titulo: 'Crear test'})
+})
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use('/api', blogRouter)
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).render('404', { titulo: '404' });
+});
