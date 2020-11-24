4c4d3b6c969a98a0b3692b7758d6761ee2a1dcbf
aleix <al3ixgh@gmail.com>
2020-11-24 10:26:25 +0100

    asdgfasf

--- app.js
+++ app.js
@@ -1,54 +1,95 @@
-const express = require('express');
-const Blog = require('./models/blog');
+const express = require('express')
 
-require('./db/mongoose')
-const blogRouter = require('./routers/blog')
+require('../db/mongoose')
+const Blog = require('../models/blog') // optional
+const blogRouter = require('../routers/blog')
 
 const port = process.env.PORT
 
 // express app
-const app = express();
+const app = express()
 
 // listen for requests
 app.listen(port, () => {
   console.log(`Server listening to port ${port}`)
-});
-
-// register view engine
-app.set('view engine', 'ejs');
-
-// middleware & static files
-app.use(express.static('public'));
-
-app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
-app.use(express.json()); // Parse JSON bodies
-
-app.get('/', async (req, res) => {
-  try{
-    const blogs =await Blog.find({})
-    res.render('index', { blogs: blogs, titulo: 'Índice' });
-  }catch (e){
-
-  res.render('index', { blogs: [], titulo: 'Índice' });
-  }
-});
-
-app.get('/form', (req, res) => {
-  res.render('formulario', { titulo: 'crear blog' });
-});
-
-
-
-app.post('/form', (req,res) => {
-  res.redirect(307, './')
 })
 
+// register view engine
+app.set('view engine', 'ejs')
+
+// middleware & static files
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+
+// web server
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Home' })
+});
+
+// api's
 app.use(express.json())
 app.use('/api', blogRouter)
 
-
-
 // 404 page
 app.use((req, res) => {
-  res.status(404).render('404', { titulo: '404' });
-});
+  res.status(404).render('404', { title: '404' })
+})
+
+
+
+//------------------------------------------------------------------
+
+// const express = require('express');
+// const Blog = require('./models/blog');
+
+// require('./db/mongoose')
+// const blogRouter = require('./routers/blog')
+
+// const port = process.env.PORT
+
+// // express app
+// const app = express();
+
+// // listen for requests
+// app.listen(port, () => {
+//   console.log(`Server listening to port ${port}`)
+// });
+
+// // register view engine
+// app.set('view engine', 'ejs');
+
+// // middleware & static files
+// app.use(express.static('public'));
+
+// app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
+// app.use(express.json()); // Parse JSON bodies
+
+// app.get('/', async (req, res) => {
+//   try{
+//     const blogs =await Blog.find({})
+//     res.render('index', { blogs: blogs, titulo: 'Índice' });
+//   }catch (e){
+
+//   res.render('index', { blogs: [], titulo: 'Índice' });
+//   }
+// });
+
+// app.get('/form', (req, res) => {
+//   res.render('formulario', { titulo: 'crear blog' });
+// });
+
+
+
+// app.post('/form', (req,res) => {
+//   res.redirect(307, './')
+// })
+
+// app.use(express.json())
+// app.use('/api', blogRouter)
+
+
+
+// // 404 page
+// app.use((req, res) => {
+//   res.status(404).render('404', { titulo: '404' });
+// });
